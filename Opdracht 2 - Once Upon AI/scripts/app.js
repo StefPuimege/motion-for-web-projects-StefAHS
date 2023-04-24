5a9f2a10b1db1fffd99d7e83bddc713514d453a0
StefAHS <stefpuim@student.arteveldehs.be>
2023-04-24 16:11:37 +0200

    Opdr 2 - V3

--- Opdracht 2 - Once Upon AI/scripts/app.js
+++ Opdracht 2 - Once Upon AI/scripts/app.js
@@ -4,13 +4,27 @@ chapters.forEach((chapter) => {
     gsap.from(chapter, {
         scrollTrigger: {
             trigger: chapter,
-            scroller: "parallax",
+            scroller: ".parallax",
             toggleActions: "play reverse play reverse",
             start: "top 90%",
             end: "bottom 10%"
         },
-        duration: 1,
-        y: -300,
+        duration: 0.3,
+        scale: 0.6,
         opacity: 0
     });
 });
+
+gsap.from(".timeline ul"), {
+    duration: 0.3,
+    scale: 0.6,
+    opacity: 0,
+    x: -100,
+    stagger: 1
+};
+
+gsap.from("#title .ch"), {
+    duration: 0.3,
+    scale: 0.6,
+    opacity: 0,
+};
