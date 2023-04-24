ecdaafc03201c828edbc366a873b5c5e848e3e13
StefAHS <stefpuim@student.arteveldehs.be>
2023-04-24 13:10:15 +0200

    Opdracht 2 - V1

---
+++ Opdracht 2 - Once Upon AI/scripts/app.js
@@ -0,0 +1,15 @@
+let chapters = gsap.utils.toArray(".ch");
+
+chapters.forEach((chapter) => {
+    gsap.from(chapter, {
+        scrollTrigger: {
+            trigger: chapter,
+            toggleActions: "play reverse play reverse",
+            start: "top 90%",
+            end: "bottom 10%"
+        },
+        duration: 1,
+        y: -300,
+        opacity: 0
+    });
+});
